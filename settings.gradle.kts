77962f2d865223c7d6d9ccaf409f16609bdd496e
Stefan Thaler <stefan@thaler-it.net>
2023-06-26 09:41:15 +0200

    Add snapshot repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,8 @@ dependencyResolutionManagement {
         mavenCentral()
         google()
         mavenLocal()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
 
     versionCatalogs {
