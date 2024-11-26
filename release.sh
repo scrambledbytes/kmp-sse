27d37ebfe19b8587cb5e66dbbbc568939401fea8
Stefan Thaler <stefan@thaler-it.net>
2024-11-26 13:08:31 +0100

    No configuration cache for release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 # https://github.com/gradle-nexus/publish-plugin#publishing-and-closing-in-different-gradle-invocations
-./gradlew publishToSonatype
-./gradlew findSonatypeStagingRepository closeSonatypeStagingRepository
-./gradlew findSonatypeStagingRepository releaseSonatypeStagingRepository
+./gradlew publishToSonatype --no-configuration-cache
+./gradlew findSonatypeStagingRepository closeSonatypeStagingRepository --no-configuration-cache
+./gradlew findSonatypeStagingRepository releaseSonatypeStagingRepository --no-configuration-cache
